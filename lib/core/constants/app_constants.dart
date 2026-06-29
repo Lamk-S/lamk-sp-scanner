fb4ee55bd8171cbd603cc6361fd0081d8fee5502
Lamk-S <kunlancelot@gmail.com>
2026-06-28 23:13:18 -0500

    chore: implement core architectural components (constants, theme, result)

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String appName = 'ScanNeo POS';
+  static const String apiBaseUrl = 'http://10.246.240.25:8000';
+  static const Duration apiTimeout = Duration(seconds: 10);
+  static const Duration scanCooldown = Duration(milliseconds: 1500);
+}
---
+++ lib/core/utils/result.dart
@@ -0,0 +1,15 @@
+sealed class Result<T> {
+  const Result();
+}
+
+final class Success<T> extends Result<T> {
+  final T data;
+  const Success(this.data);
+}
+
+final class Failure<T> extends Result<T> {
+  final String message;
+  final Object? error;
+  final StackTrace? stackTrace;
+  const Failure(this.message, {this.error, this.stackTrace});
+}
